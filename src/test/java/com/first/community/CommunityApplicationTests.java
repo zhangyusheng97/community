cd48a8c9477b23ae34926b22b7f020199ba51535
zhangyusheng97 <836636182@qq.com>
2020-10-14 10:58:29 +0800

    first community

---
+++ src/test/java/com/first/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
